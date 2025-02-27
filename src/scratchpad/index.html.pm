63583a07c3952772f23557d6a52a7621a0940744
Stone Liu <liu.sto@northeastern.edu>
2025-02-27 07:50:23 -0500

    Insert cool quote

--- src/scratchpad/index.html.pm
+++ src/scratchpad/index.html.pm
@@ -6,6 +6,8 @@
 ◊(require pollen/pagetree pollen/template sugar/coerce)
 ◊(let () (current-pagetree (load-pagetree "../index.ptree")) "")
 
+◊em{The best way to learn something is to go and do it. Doing it wrong the first time is better than doing it right the first time.}
+
 ◊items{
   ◊item{◊link["/scratchpad/leetcode.html"]{leetcode}}
 }
