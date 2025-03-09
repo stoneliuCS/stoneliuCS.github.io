370d6d6ccaa345e524b43e1ea60244e1cf8316ab
Stone Liu <liu.sto@northeastern.edu>
2025-03-09 13:00:22 -0400

    Compiler Chapter 1, Semantics.

---
+++ src/scratchpad/compiler.html.pm
@@ -0,0 +1,12 @@
+#lang pollen
+◊headline{Writing a Compiler?}
+
+◊p{
+  A link to the current github project is provided ◊link["https://github.com/stoneliuCS/compiler/tree/main"]{here}.
+}
+
+◊smaller-headline{Chapter 1}
+
+◊items{
+  ◊item{◊link["/scratchpad/compiler/starting.html"]{StoneScript}}
+}
--- src/scratchpad/index.html.pm
+++ src/scratchpad/index.html.pm
@@ -9,4 +9,5 @@
 
 ◊items{
   ◊item{◊link["/scratchpad/leetcode.html"]{leetcode}}
+  ◊item{◊link["/scratchpad/compiler.html"]{writing a crappy compiler}}
 }
