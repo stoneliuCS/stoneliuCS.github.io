b7753749c44e7313de9cf2b6cb865fb835f6ab6a
Stone Liu <liu.sto@northeastern.edu>
2025-03-15 15:29:24 -0400

    Update this

--- src/scratchpad/interpreter.html.pm
+++ src/scratchpad/interpreter.html.pm
@@ -2,7 +2,7 @@
 ◊headline{Writing an Interpreter.}
 
 ◊p{
-  A link to the current github project is provided ◊link["https://github.com/stoneliuCS/compiler/tree/main"]{here}.
+  A link to the current github project is provided ◊link["https://github.com/stoneliuCS/compiler-interpreter/tree/main"]{here}.
 }
 
 ◊smaller-headline{Chapter 1}
