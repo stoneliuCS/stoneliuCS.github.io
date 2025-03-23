74fc1dd04f37c6bf9d36a93cbb72112b67ed74e5
Stone Liu <liu.sto@northeastern.edu>
2025-03-23 19:27:52 -0400

    Lexer

--- src/scratchpad/interpreter/starting.html.pm
+++ src/scratchpad/interpreter/starting.html.pm
@@ -6,6 +6,8 @@
   From my understanding a compiler is just a ◊em{very, very, very} sophisticated function.
 
   In essence it takes source code, and translates that into a ◊em{semantically} equivalent form in the target language.
+
+  The compiler usually outputs an executable in machine code. This greatly varies depending on computer architectures. Compilers that compile to another high level programming language is called a transpiler.
 }
 
 ◊code-block["racket"]{
@@ -27,3 +29,21 @@
   var y <- 2;
   var z <- x + y;
 }
+
+◊p{
+  Lets break down at the highest level where to start. Every compiler/interpreter relies on a ◊em{frontend}. This is what parses the source code into a more easily managable data structure called ◊em{tokens}.
+
+  At this rate, I forsee at the top level of my lexer doing this:
+
+  ◊code-block["c"]{
+    token_list *tokenize(const char *source) {
+      token_ctx ctx = ...
+      while (!is_end(ctx)) {
+        ...
+        scan_token(ctx);
+      }
+      return tokens;
+    }
+  }
+}
+
