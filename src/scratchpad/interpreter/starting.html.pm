2a67481281342fa39ba2465bfc3a0a83ffce8b0a
Stone Liu <liu.sto@northeastern.edu>
2025-03-23 19:32:16 -0400

    Update lexer

--- src/scratchpad/interpreter/starting.html.pm
+++ src/scratchpad/interpreter/starting.html.pm
@@ -34,7 +34,6 @@
   Lets break down at the highest level where to start. Every compiler/interpreter relies on a ◊em{frontend}. This is what parses the source code into a more easily managable data structure called ◊em{tokens}.
 
   At this rate, I forsee at the top level of my lexer doing this:
-
   ◊code-block["c"]{
     token_list *tokenize(const char *source) {
       token_ctx ctx = ...
@@ -44,6 +43,25 @@
       }
       return tokens;
     }
+  }
+  What is a ◊em{token_ctx} you might ask? Good question, since C doesn't really support object oriented programming, I need to have a struct which contains all the necessary information for all my helper functions throughout parsing.
+  ◊code-block["c"]{
+    // A Token Context is Structured Data
+    // It holds
+    // A starting index
+    // A Current index
+    // The current line number
+    // The current source program.
+    // The current running list of tokens.
+    // A flag to see if the source program has errors.
+    typedef struct token_ctx {
+      int start;
+      int current;
+      int line;
+      const char *source;
+      token_list_t *tokens;
+      bool error;
+    } token_ctx;
   }
 }
 
